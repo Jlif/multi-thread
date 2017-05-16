86822d744407a92199b34492dabebfd0d96c5564
jiangchen <1760247442@qq.com>
2017-05-16 13:57:56 +0800

    Java多线程 await()

---
+++ src/ch4/t09_fair_noFair_test/Service.java
@@ -0,0 +1,21 @@
+package ch4.t09_fair_noFair_test;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Service {
+	private ReentrantLock lock;
+
+	public Service(boolean isFair) {
+		super();
+		lock = new ReentrantLock(isFair);
+	}
+
+	public void serviceMethod() {
+		try {
+			lock.lock();
+			System.out.println("ThreadName=" + Thread.currentThread().getName() + "获得锁定");
+		} finally {
+			lock.unlock();
+		}
+	}
+}
